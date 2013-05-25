9e3ac456e539890148c8cf0aca057f8c083af7a1
Gion-Martin Pfiffner <terasu88@gmail.com>
2013-05-25 14:52:35 +0200

    zahlreiche änderungen
    zahlreiche änderungen

---
+++ FIS_Heer/src/main/java/com/armee/fis_heer/About.java
@@ -0,0 +1,15 @@
+package com.armee.fis_heer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Gion-martin on 25.05.13.
+ */
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+}
+}
---
+++ FIS_Heer/src/main/java/com/armee/fis_heer/Main.java
@@ -0,0 +1,9 @@
+package com.armee.fis_heer;
+
+import android.app.Activity;
+
+/**
+ * Created by Gion-martin on 25.05.13.
+ */
+public class Main extends Activity {
+}
